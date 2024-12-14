e46d444283949b5295b2865254fee4f4fbfc8ed0
Benson Ye <bye33353@gmail.com>
2024-12-13 21:41:20 -0500

    Added initial path generation.

--- Assets/Scripts/MazeGenerator.cs
+++ Assets/Scripts/MazeGenerator.cs
@@ -1,5 +1,6 @@
 using System.Collections.Generic;
-using System.Numerics;
+using System.Linq;
+using System.Threading.Tasks;
 using UnityEngine;
 
 public class MazeGenerator : MonoBehaviour{
@@ -12,11 +13,14 @@ public class MazeGenerator : MonoBehaviour{
 
     public Cell[,] mazeData;
     public Dictionary<(int, int), Cell> openList;
-    public Dictionary<(int, int), Cell> closedList;
+    public List<Cell> closedList;
     
     void Start(){
+        openList = new Dictionary<(int, int), Cell>();
+        closedList = new List<Cell>();
         InitCells();
         GenerateMaze();
+        DrawMaze();
     }
 
     void InitCells(){
@@ -32,41 +36,71 @@ public class MazeGenerator : MonoBehaviour{
     // Chooses a random cell in openList to act as the first part of the maze.
     // Finds another random cell to act as the starting cell.
     // While the current cell is not the starting cell,
-    void GenerateMaze(){
-        closeCell(getRandom());
+    private void GenerateMaze(){
+        Cell endCell = getRandom();
+        closedList.Add(endCell);
         Cell currentCell = getRandom();
-        while (closedList.ContainsValue(currentCell)){
-            Cell nextCell = performRandomWalk(currentCell);
-            currentCell.nextCell = nextCell;
-            closeCell(currentCell);
+
+        List<Cell> path = FindPath(currentCell);
+        List<Cell> fixedPath = new List<Cell>{currentCell};
+        currentCell = path[0];
+        while (!closedList.Any(c => c.position == currentCell.position)){
+            Cell nextCell = currentCell.nextCell;
+            fixedPath.Add(currentCell);
             currentCell = nextCell;
+        }
+
+        foreach (Cell cell in fixedPath){
+            closedList.Add(cell);
+        }
+    }
+
+    // Finds path by determining a starting cell and applying a random walk. When the next cell is in the maze, the path is created.
+    // If the path loops upon itself, the current loop is reset.
+    // Returns a list of cells.
+    private List<Cell> FindPath(Cell startingCell){
+        List<Cell> path = new List<Cell>();
+        while (!closedList.Any(c => c.position == startingCell.position)){
+            Cell nextCell = performRandomWalk(startingCell);
+            if (path.Any(c => c.position == nextCell.position)){
+                startingCell = path[0];
+                path.Clear();
+                path.Add(startingCell);
+                continue;
+            }
+            startingCell.nextCell = nextCell;
+            path.Add(startingCell);
+            startingCell = nextCell;
+        }
+        return path;
+    }
+
+    private void DrawMaze(){
+        foreach (Cell cell in closedList){
+            Vector3 position = new Vector3(cell.position.x, 0f, cell.position.y);
+            //Instantiate(cellPrefab, position, Quaternion.identity);
+            if (cell.isWall)
+                Instantiate(wallPrefab, position, Quaternion.identity);
         }
     }
 
     // Gets a random cell from openList.
     // Returns the random cell.
-    Cell getRandom(){
+    private Cell getRandom(){
         List<Cell> cells = new List<Cell>(openList.Values);
         return cells[Random.Range(0, cells.Count)];
-    }
-    
-    // Removes cell from openList and moves it to closedList.
-    void closeCell(Cell cell){
-        openList.Remove((cell.position.x, cell.position.y));
-        closedList.Add((cell.position.x, cell.position.y), cell);   
     }
 
     // When provided a cell, chooses a random neighboring cell that is not yet chosen to go into.
     // Returns the chosen cell.
-    Cell performRandomWalk(Cell previousCell){
+    private Cell performRandomWalk(Cell previousCell){
         List<Cell> neighbors = getNeighbors(previousCell);
-        Cell nextCell = neighbors[Random.Range(0, neighbors.Count)];
-        return nextCell;
+        return neighbors[Random.Range(0, neighbors.Count)];
     }
 
     // Checks if there are any open cells in the 4 directions of the current cell - Up, Right, Down, Left.
     // Returns a list of cells that are open.
-    List<Cell> getNeighbors(Cell cell){
+    private List<Cell> getNeighbors(Cell cell){
         List<Cell> cells = new List<Cell>();
         if (openList.ContainsKey((cell.position.x, cell.position.y + 1)))
             cells.Add(openList[(cell.position.x, cell.position.y + 1)]);
@@ -75,7 +109,8 @@ public class MazeGenerator : MonoBehaviour{
         if (openList.ContainsKey((cell.position.x, cell.position.y - 1)))
             cells.Add(openList[(cell.position.x, cell.position.y - 1)]);       
         if (openList.ContainsKey((cell.position.x - 1, cell.position.y)))
-            cells.Add(openList[(cell.position.x - 1, cell.position.y)]);            
+            cells.Add(openList[(cell.position.x - 1, cell.position.y)]);
+
         return cells;
     }
 
